27088fbb0229bad7728aed5452d3a2fd8f5a29e2
Bibek Kumar panda <119118569+Bibek-9078@users.noreply.github.com>
2024-01-20 22:38:48 +0530

    Add files via upload

---
+++ components/component_style/style_css.py
@@ -0,0 +1,15 @@
+"""
+CSS for custom styling
+
+Author: Abhishek Gupta
+Github: https://github.com/1abhi6
+"""
+
+PADDING_TOP = """
+<style>
+        .main > div {
+            padding-top: 2rem;
+            padding-bottom: 2rem;
+        }
+    </style>
+"""
